7fda457c9238c1efc0c52d05b6047c1a221d7b5f
Tim Westberg <timwestberg43@gmail.com>
2018-12-10 20:23:06 -0800

    Continue reintegrating with Materilize the Main profile home page is nearly redesigned, adding different functionality building with mobile first in mind

--- controllers/portfolio_controller.js
+++ controllers/portfolio_controller.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
     // Must be placed in an object varible to be accessed correctly, shows up as objects otherwise
     const hbsObject = {
       style: "index",
-      jsFile:"main",
+      jsFile:"portfolio",
       title:"Tim Westberg"
     };
 
--- public/assets/js/portfolio.js
+++ public/assets/js/portfolio.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     $('.materialboxed').materialbox();
   });
+
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
